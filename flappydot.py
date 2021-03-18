c44b6552cc8dac95c9c60268e3d17917304446ee
tboonma <tboonma@hotmail.com>
2021-03-18 10:47:41 +0700

    Resolve #8

--- flappydot.py
+++ flappydot.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox 
 
 from gamelib import Sprite, GameApp, Text
 
@@ -26,6 +27,11 @@ class Dot(Sprite):
 
     def jump(self):
         self.vy = JUMP_VELOCITY
+
+    def is_out_of_screen(self):
+        if self.x <= CANVAS_WIDTH and self.y <= CANVAS_HEIGHT:
+            return False
+        return True
     
 
 class FlappyGame(GameApp):
@@ -45,21 +51,33 @@ class FlappyGame(GameApp):
         pass
 
     def post_update(self):
-        pass
+        if self.dot.is_out_of_screen() and self.is_started:
+            messagebox.showinfo("Alert", "Game Over!")
+            self.dot.is_started = False
+            self.pillar_pair.is_started = False
+            self.is_started = False
 
     def on_key_pressed(self, event):
         if event.char == ' ':
             if not self.is_started:
                 self.is_started = True
                 self.dot.start()
+                self.pillar_pair.start()
             else:
                 self.dot.jump()
 
 
 class PillarPair(Sprite):
+    def init_element(self):
+        self.is_started = False
+
     def update(self):
-        self.vx = PILLAR_SPEED
-        self.x -= self.vx
+        if self.is_started:
+            self.vx = PILLAR_SPEED
+            self.x -= self.vx
+    
+    def start(self):
+        self.is_started = True
 
 
 if __name__ == "__main__":
